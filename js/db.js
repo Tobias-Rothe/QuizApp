ec4cf17ca04bbfed0b8fd06453766b4807232bff
Tobias Rothe <tobiasrothe77@gmail.com>
2024-09-17 12:05:38 +0200

    added questions

---
+++ js/db.js
@@ -0,0 +1,56 @@
+let questions = 
+{
+    "questions": [
+      {
+        "question": "Was bedeutet HTML?",
+        "answers": [
+          "Hyper Text Markup Language",
+          "Hyperlinks and Text Markup Language",
+          "Home Tool Markup Language",
+          "Hyperlinking Text Marking Language"
+        ],
+        "correctAnswer": 0
+      },
+      {
+        "question": "Welche CSS-Eigenschaft wird verwendet, um den Text fett darzustellen?",
+        "answers": [
+          "font-weight",
+          "text-style",
+          "font-bold",
+          "text-weight"
+        ],
+        "correctAnswer": 0
+      },
+      {
+        "question": "Wie bindet man eine externe JavaScript-Datei in HTML ein?",
+        "answers": [
+          "<script href='script.js'></script>",
+          "<script src='script.js'></script>",
+          "<link rel='script' href='script.js'>",
+          "<script link='script.js'></script>"
+        ],
+        "correctAnswer": 1
+      },
+      {
+        "question": "Welche der folgenden CSS-Einheiten ist relativ zur Schriftgröße des Elements?",
+        "answers": [
+          "px",
+          "em",
+          "cm",
+          "mm"
+        ],
+        "correctAnswer": 1
+      },
+      {
+        "question": "Welche Methode wird verwendet, um in JavaScript eine Zeichenkette zu teilen?",
+        "answers": [
+          "slice()",
+          "split()",
+          "splice()",
+          "substring()"
+        ],
+        "correctAnswer": 1
+      }
+    ]
+  }
+  
